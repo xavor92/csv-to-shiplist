2b1454c4beff39387136ea6e13c33e9f55cd7fb3
xavor92 <owestermann@googlemail.com>
2017-07-24 19:50:00 +0200

    added check for empty lines

--- csv-to-shiplist.py
+++ csv-to-shiplist.py
@@ -7,29 +7,34 @@ def isComment(row):
         return True
     return False
 
-def parse(csv_file):
-    csv_reader = csv.reader(csv_file, delimiter=';')
+def hasContent(row):
+    for element in row:
+        if element:
+            return True
+    return False
+
+def parse(input, output):
+    csv_reader = csv.reader(input, delimiter=';')
     validate = formatting.validator()
     formatter = formatting.formatter()
 
-    output = ""
-
     try:
         for row in csv_reader:
-            if not isComment(row):
-                if row[1]:
-                    # ship, check data
-                    if validate.checkShip(row):
-                        formatter.ship(row)
-                    else:
-                        print "In row", csv_reader.line_num, row
+            if hasContent(row):
+                if not isComment(row):
+                    if row[1]:
+                        # ship, check data
+                        if validate.checkShip(row):
+                            formatter.ship(row)
+                        else:
+                            print "In row", csv_reader.line_num, row
 
-                else:
-                    # group!
-                    formatter.group(row)
+                    else:
+                        # group!
+                        formatter.group(row)
         formatter.end()
 
-        print formatter.output
+        output.write(formatter.output)
     
     except ValueError as e:
         print "Error on Line {}: {}".format(csv_reader.line_num, e)
@@ -39,7 +44,8 @@ def main():
     print "csv-to-shiplist.py"
     if ( len(sys.argv) == 2 ) and ( sys.argv[1].endswith("csv") ):
         csv_file = open(sys.argv[1], 'rb')
-        parse(csv_file)
+        outfile = open("output.txt", "wb")
+        parse(csv_file, outfile)
     else:
         print "add path to input data as first argument"
 
